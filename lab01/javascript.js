1d50559efde02f7e8b0db0bd45ce2ed81c672702
JoeVenton <joe.venton@gmail.com>
2023-06-14 16:41:06 +0100

    completed js logic

--- lab01/javascript.js
+++ lab01/javascript.js
@@ -3,41 +3,35 @@ let monthNumber =""
 let monthText =""
 let birthYear =""
 let finalAge =""
+let monthCheck =""
 let ageCalc =""
 function userInfo() {
     userName = prompt("Hi there! What is you name?");
-    console.log(userName);
-    if (userName == "") { 
+    if (userName =="") {
         alert("Come on! Don't be shy!");
-        userInfo();
+        userName = prompt("Hi there! What is you name?");
     }
     else {
-    userMonth();
+        userAge();
     }
 }
-function userMonth() {
-    let monthNumber = prompt("It's great to meet you " + userName + "! \nMay I ask what month you were born in?\nPlease enter a number corresponding to the month of the year! (1-12)", 1);
-    console.log(monthNumber);
-    const months = ["a month that you want to keep secret?", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function userAge() {
+    monthNumber = prompt("It's great to meet you " + userName + "! \nMay I ask what month you were born in?\nPlease enter a number corresponding to the month of the year! (1-12)", 1);
     if (monthNumber >= 0 && monthNumber <= 12) {
-        let monthText = (months)[monthNumber];
         const date = new Date();
-        console.log(date);
-        var month = date.getMonth();
-        console.log(Number(month));
-        let monthCheck = Number((months)[month]);
-        console.log(monthText);
-        console.log(Number(monthCheck));
-        let birthYear = prompt("In what year were you born, " + userName + "?");
-        console.log(Number(birthYear));
+        const months = ["a month that you want to keep secret?", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let monthText = months[monthNumber];
+        let birthYear = prompt("Cool! So you were born in " + monthText + "!\nIn what year were you born, " + userName + "?");
+        var month = date.getMonth() + 1;
         var year = date.getFullYear();
-        console.log(Number(year));
-        let ageCalc = (year - birthYear) + (month - monthCheck);
-        console.log(Number(ageCalc));
+        let monthMod = (month - monthNumber);
+        monthMod = monthMod < 0 ? -1 : monthMod;
+        monthMod = monthMod > 1 ? 0 : monthMod;
+        let ageCalc = (year - (Number(birthYear) - monthMod));
         alert("Wow! No Way! That makes you " + ageCalc + " years old!?\nLooking good! You don't look a day over " + (ageCalc-1) + "!")
     }
     else {
-        alert("That's not a month, you silly goose!")
-        userMonth();
+        alert("That's not a month, you silly goose!");
+        userAge();
     }
 }
